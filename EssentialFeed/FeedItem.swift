b60831d90c622c5bf893c662deea6384a58696c8
Hai Phung <hai.phung@peoplemedia.com>
2020-04-01 22:11:44 -0500

    Create the FeedItem model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hai Phung on 4/1/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let lcoation: String?
+    let imageURL: URL
+}
